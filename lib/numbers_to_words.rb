07f01115bd167da70eebc35378100eae05e40bd8
Justin Scott & Brigham Thompson <student@epicodus.com>
2015-08-04 15:21:52 -0700

    Passed first spec, single digit to word

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,5 +1,7 @@
-class String
+class Fixnum
   define_method(:numbers_to_words) do
+    single_digit = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five" }
 
+    single_digit.fetch(self)
   end
 end
