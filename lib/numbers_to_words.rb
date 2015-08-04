7f3dc607d0bf44648a24c53aa1036f3a8c81323a
Justin Scott & Brigham Thompson <student@epicodus.com>
2015-08-04 15:37:06 -0700

    passed second spec, teens into word

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,7 +1,12 @@
 class Fixnum
   define_method(:numbers_to_words) do
-    single_digit = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five" }
+    single_digits = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
+    teen_digits = {10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
 
-    single_digit.fetch(self)
+    if single_digits.include?(self)
+      single_digits.fetch(self)
+    else
+      teen_digits.fetch(self)
+    end
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -5,4 +5,8 @@ describe ('Fixnum#numbers_to_words') do
   it('convert single digit into written word') do
     expect((5).numbers_to_words()).to(eq("five"))
   end
+
+  it('converts double digit into written word') do
+    expect((15).numbers_to_words()).to(eq("fifteen"))
+  end
 end
