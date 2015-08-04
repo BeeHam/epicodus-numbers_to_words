d5ffc9d02ffad97bd5a0ff48a083f1965b1ae460
Justin Scott & Brigham Thompson <student@epicodus.com>
2015-08-04 15:41:05 -0700

    Wrote third spec

--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -6,7 +6,11 @@ describe ('Fixnum#numbers_to_words') do
     expect((5).numbers_to_words()).to(eq("five"))
   end
 
-  it('converts double digit into written word') do
+  it('converts teen digit into written word') do
     expect((15).numbers_to_words()).to(eq("fifteen"))
+  end
+
+  it('converts double digit into written word') do
+    expect((22).numbers_to_words()).to(eq("twenty two"))
   end
 end
